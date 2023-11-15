a320d9a2344e02b5f5efe5381c671716114cd86b
jden6 <jykim6.cloud@gmail.com>
2023-11-15 14:31:51 +0900

    testing

--- src/app/(home)/notice/page.tsx
+++ src/app/(home)/notice/page.tsx
@@ -1,4 +1,7 @@
-const NoticePage = () => {
+import { getServerSession } from 'next-auth'
+
+const NoticePage = async () => {
+  const session = await getServerSession()
   return <div>
     notice page
   </div>
