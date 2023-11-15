fb815cb4a3f9e82073eead100f152fee64d5a827
jden6 <jykim6.cloud@gmail.com>
2023-11-15 13:05:06 +0900

    add naver, google auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,21 @@
 import NextAuth, { NextAuthOptions } from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+import NaverProvider from 'next-auth/providers/naver'
+import GoogleProvider from 'next-auth/providers/google'
 
 const authOptions: NextAuthOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID ?? '',
       clientSecret: process.env.GITHUB_SECRET || ''
+    }),
+    NaverProvider({
+      clientId: process.env.NAVER_ID ?? '',
+      clientSecret: process.env.NAVER_SECRET || ''
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID ?? '',
+      clientSecret: process.env.GOOGLE_SECRET || ''
     })
   ],
 }
